20daf933c56c1504115c742b739de62a9a5e8bb1
Daan van den Bergh <18595395+Dan0sz@users.noreply.github.com>
2020-01-06 09:49:32 +0100

    Update and rename caos-google-adwords.php to caos-google-ads.php

--- caos-google-ads.php
+++ caos-google-ads.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * @formatter:off
- * Plugin Name: Google Adwords for CAOS
+ * Plugin Name: Google Ads for CAOS
  * Plugin URI: https://daan.dev/google-adwords-caos/
- * Description: Add Google Optimize for CAOS in gtag.js
+ * Description: Track Google Ads conversions with CAOS.
  * Version: 1.0.0
  * Author: Daan van den Bergh
  * Author URI: https://daan.dev
@@ -16,20 +16,20 @@ if (!defined('ABSPATH')) {
 }
 
 /**
- * Replace 'YOUR-ADWORDS-ID' with, well, your Adwords ID.
+ * Replace 'YOUR-ADS-ID' with, well, your Google Ads ID.
  */
-function caos_google_adwords()
+function caos_google_ads()
 {
-    $adwordsId = 'YOUR-ADWORDS-ID';
+    $adsId = 'YOUR-ADS-ID';
 
     if (CAOS_OPT_REMOTE_JS_FILE == 'gtag.js') {
         add_filter(
             'caos_gtag_additional_config',
-            function() use ($adwordsId) {
-                return "gtag('config', '$adwordsId');";
+            function() use ($adsId) {
+                return "gtag('config', '$adsId');";
             }
         );
     }
 }
 
-add_action('caos_process_settings', 'caos_google_adwords');
+add_action('caos_process_settings', 'caos_google_ads');

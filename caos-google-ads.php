d478b92fbed6c2afd5e0ed0a9102e8423b22d770
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2024-02-08 09:08:24 +0100

    Fixed: caos_gtag_additional_config is an action, not a filter.

--- caos-google-ads.php
+++ caos-google-ads.php
@@ -11,23 +11,22 @@
  * @formatter:on
  */
 
-if (!defined('ABSPATH')) {
-    exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
 /**
  * Replace 'YOUR-ADS-ID' with, well, your Google Ads ID.
  */
-function caos_google_ads()
-{
-    $ads_id = 'YOUR-ADS-ID';
+function caos_google_ads() {
+	$ads_id = 'YOUR-ADS-ID';
 
-    add_filter(
-        'caos_gtag_additional_config',
-        function() use ($ads_id) {
-            return "gtag('config', '$ads_id');";
-        }
-    );
+	add_action(
+		'caos_gtag_additional_config',
+		function () use ( $ads_id ) {
+			return "gtag('config', '$ads_id');";
+		}
+	);
 }
 
-add_action('caos_process_settings', 'caos_google_ads');
+add_action( 'caos_process_settings', 'caos_google_ads' );

fae995706154d91650a9d6e937d6c731f3d5c8d9
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2024-02-08 09:13:56 +0100

    1.0.2

--- caos-google-ads.php
+++ caos-google-ads.php
@@ -4,7 +4,7 @@
  * Plugin Name: Google Ads for CAOS
  * Plugin URI: https://daan.dev/google-adwords-caos/
  * Description: Track Google Ads conversions with CAOS.
- * Version: 1.0.1
+ * Version: 1.0.2
  * Author: Daan van den Bergh
  * Author URI: https://daan.dev
  * License: GPL2v2 or later
@@ -16,17 +16,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Replace 'YOUR-ADS-ID' with, well, your Google Ads ID.
+ * To add support for your Google Ads account, simply add the highlighted line to your Google tag, replacing “TAG_ID” with the unique ID supplied to
+ * you when you set up your conversion tracking tags.
+ * @see https://support.google.com/google-ads/answer/7548399?hl=en
+ * @return void
  */
 function caos_google_ads() {
 	$ads_id = 'YOUR-ADS-ID';
 
-	add_action(
-		'caos_gtag_additional_config',
-		function () use ( $ads_id ) {
-			return "gtag('config', '$ads_id');";
-		}
-	);
+	echo "gtag('config', '$ads_id');";
 }
 
-add_action( 'caos_process_settings', 'caos_google_ads' );
+add_action( 'caos_gtag_additional_config', 'caos_google_ads' );
